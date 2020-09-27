7bd4a035cbbbd27e77c9132cbbc08a3cedf02c39
mpinchuk1 <51299935+mpinchuk1@users.noreply.github.com>
2020-09-28 00:48:58 +0300

    fix filter

--- src/main/java/org/appMain/controllers/ShopController.java
+++ src/main/java/org/appMain/controllers/ShopController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,9 +18,10 @@ public class ShopController {
         this.shopService = shopService;
     }
 
-    @GetMapping("filterExpired")
-    public ResponseEntity<Void> filterExpiredProducts(){
-        shopService.filterExpiredProducts();
+    @GetMapping
+    public ResponseEntity<Void> filterExpiredProducts(@RequestParam boolean filter){
+        if(filter)
+            shopService.filterExpiredProducts();
         return ResponseEntity.ok().build();
     }
 
